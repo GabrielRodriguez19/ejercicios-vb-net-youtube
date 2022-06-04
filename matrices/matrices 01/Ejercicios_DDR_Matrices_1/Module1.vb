75962deabc16f4107f21741615e5d1f52e479039
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-04 13:31:41 +0200

    matrices 1 vb.net

---
+++ matrices/matrices 01/Ejercicios_DDR_Matrices_1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Creo la mnatriz de 3x3
+        Dim matriz(2, 2) As Integer
+
+        'Recorro la matriz
+        For i = 0 To matriz.GetLength(0) - 1
+            For j = 0 To matriz.GetLength(1) - 1
+                'Le doy valor a la posicion de la matriz
+                matriz(i, j) = (i * matriz.GetLength(0)) + (j + 1)
+                Console.Write(matriz(i, j) & " ") 'Lo muestro
+
+            Next
+            Console.WriteLine() 'Salto de linea
+        Next
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ matrices/matrices 01/Ejercicios_DDR_Matrices_2/Module1.vb
@@ -0,0 +1,62 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Pedimos el numero de filas
+        Console.WriteLine("Escribe el numero de filas")
+        Dim filas As Integer = Convert.ToInt32(Console.ReadLine())
+
+        'Restamos al numero de filas 1 para que se compense
+        Dim matriz(filas - 1, 1) As String
+
+        'Para mayor comodidad, os dejo ambas versiones en el mismo fichero
+        'Solo teneis que comentar una parte y descomentar la otra
+
+        'Version 1
+
+        'For i = 0 To matriz.GetLength(0) - 1
+
+        '    Console.WriteLine("Escribe el nombre del usuario " & (i + 1))
+        '    matriz(i, 0) = Console.ReadLine()
+
+        '    Console.WriteLine("Escribe el passwors del usuario " & (i + 1))
+        '    matriz(i, 1) = Console.ReadLine()
+
+        'Next
+
+        'Version 2
+
+        For i = 0 To matriz.GetLength(0) - 1
+            For j = 0 To matriz.GetLength(1) - 1
+
+                Select Case j
+                    Case 0
+                        Console.WriteLine("Escribe el nombre del usuario " & (i + 1))
+                        matriz(i, 0) = Console.ReadLine()
+                    Case 1
+                        Console.WriteLine("Escribe el password del usuario " & (i + 1))
+                        matriz(i, 1) = Console.ReadLine()
+                End Select
+
+            Next
+        Next
+
+        Console.WriteLine()
+
+        Console.WriteLine("nombre " & " pass")
+
+        'Mostramos el estado de la matriz
+        For i = 0 To matriz.GetLength(0) - 1
+            For j = 0 To matriz.GetLength(1) - 1
+
+                Console.Write(matriz(i, j) & " ")
+
+            Next
+            Console.WriteLine()
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
